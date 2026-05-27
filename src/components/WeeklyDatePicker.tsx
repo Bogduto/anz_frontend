dd0f58775c8ba10f2d2ab0b758a1891186e22954
Bohdan <bogdanbogdanvakulenka@gmail.com>
2026-05-27 05:52:06 +0300

    fix: WeeklyDatePicker - typed onChange, remove unused plugins and onClear

--- src/components/WeeklyDatePicker.tsx
+++ src/components/WeeklyDatePicker.tsx
@@ -2,42 +2,29 @@
 import { DatePicker, ConfigProvider } from "antd";
 import dayjs from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import "dayjs/locale/en-gb";
 import { useRouter, useSearchParams } from "next/navigation";
 
 dayjs.extend(isoWeek);
-dayjs.extend(utc);
-dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 dayjs.locale("en-gb");
 
-const STORE_FORMAT = "YYYY-MM-DD"; // for URL query params
-const DISPLAY_FORMAT = "DD-MM-YYYY"; // for UI display
+const STORE_FORMAT = "YYYY-MM-DD";
+const DISPLAY_FORMAT = "DD-MM-YYYY";
 
 const WeeklyDatePicker = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const startParam = searchParams.get("start");
-  // Parse using STORE_FORMAT since that's what we save in URL
   const defaultValue = startParam ? dayjs(startParam, STORE_FORMAT) : dayjs();
 
-  const onClear = () => {
-    return dayjs();
-  };
-
-  const onChange = (date) => {
+  const onChange = (date: dayjs.Dayjs | null) => {
     if (!date) return;
 
-    const start = dayjs(date.format(STORE_FORMAT), STORE_FORMAT).startOf(
-      "isoWeek",
-    );
-
     const params = new URLSearchParams(searchParams.toString());
-    params.set("start", start.format(STORE_FORMAT));
+    params.set("start", date.format(STORE_FORMAT));
 
     router.push(`?${params.toString()}`);
   };
@@ -49,11 +36,9 @@ const WeeklyDatePicker = () => {
         onChange={onChange}
         defaultValue={defaultValue}
         format={(date) => {
-          const start = dayjs(date.format(STORE_FORMAT), STORE_FORMAT).startOf(
-            "isoWeek",
-          );
-          const end = start.add(6, "day");
-          return `range ${start.format(DISPLAY_FORMAT)} - ${end.format(DISPLAY_FORMAT)} choosen ${startParam}`;
+          const weekStart = date.startOf("isoWeek");
+          const weekEnd = weekStart.add(6, "day");
+          return `${weekStart.format(DISPLAY_FORMAT)} – ${weekEnd.format(DISPLAY_FORMAT)}`;
         }}
       />
     </ConfigProvider>
